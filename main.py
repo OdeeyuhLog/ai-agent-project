2c6d710a289611bfc94b0de35f1a3ee56d59effc
Polymann <nmsantillan205@gmail.com>
2026-01-02 07:22:19 +0800

    Add multiple messages store

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=args.user_prompt,
+    contents=messages,
 )
 
 print(f"User Prompt: {args.user_prompt}")
