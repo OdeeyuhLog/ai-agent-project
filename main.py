941a1aeabce7ca6346c78600e31b643b37b79f65
Polymann <nmsantillan205@gmail.com>
2026-01-02 06:58:08 +0800

    Add gemini client and script args for user input

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-def main():
-    print("Hello from ai-agent-project!")
+import argparse
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
 
 
-if __name__ == "__main__":
-    main()
-    
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=args.user_prompt,
+)
+
+print(f"User Prompt: {args.user_prompt}")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response:\n{response.text} ")
