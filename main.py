94811a3737cc8b24ebb0f3947f873c6dd8c3f12c
Polymann <nmsantillan205@gmail.com>
2026-01-02 08:43:08 +0800

    Add logic for verbose args

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from pdb import help
 
 from dotenv import load_dotenv
 from google import genai
@@ -11,7 +12,10 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
+
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -21,7 +25,9 @@ response = client.models.generate_content(
     contents=messages,
 )
 
-print(f"User Prompt: {args.user_prompt}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 print(f"Response:\n{response.text} ")
