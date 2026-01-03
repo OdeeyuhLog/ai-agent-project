98076f6164f73fd14901fce058135c58fadb8f28
Polymann <nmsantillan205@gmail.com>
2026-01-03 11:43:04 +0800

    Add simple test cases for file content getter

---
+++ test_get_files_info.py
@@ -0,0 +1,12 @@
+from functions.get_files_info import get_files_info
+
+def test_files_get_info():
+    print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "pkg"))
+    print(get_files_info("calculator",  "/bin"))
+    print(get_files_info("calculator",  "../"))
+
+test_files_get_info()
+    
+    
+    
