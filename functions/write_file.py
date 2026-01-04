980aa6f5a7d856895a38aac65ed00514177c0142
Polymann <nmsantillan205@gmail.com>
2026-01-04 15:57:33 +0800

    Add file creation

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,3 +1,22 @@
+import os
 
 
-def write_file(working_directory, )
+def write_file(working_directory, file_path, content):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_directory, file_path))
+        os.makedirs(target_file)
+
+        if not target_file.startswith(abs_working_dir):
+            return f'Error: Cannot write to "{file_path}" as it is outside the working directory'
+
+        if not os.path.isdir(target_file):
+            f'Error: Cannot write to "{file_path}" as it is a directory '
+            
+        with open(file_path, "w") as f:
+            f.write(content)
+            
+        return f'Successfully wrote tp "{file_path}" ({len(content)} characters written)'
+
+    except Exception as e:
+        return f"Error: {str(e)}"
